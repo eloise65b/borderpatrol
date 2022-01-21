5c878e8dd3eb7be127ae3ed646d36b28cb1c68c4
Quinns <90245343+wrenntwons@users.noreply.github.com>
2022-01-21 11:09:39 +0000

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import socket
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+#assumes running on localhost: i do not have two machines to test this on but in theory
+#if you want to run generally you just need to duplicate this and send between two servers too
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
