cb24947752602e2943edec699c33abc8bfd71cf3
Quinns <90245343+wrenntwons@users.noreply.github.com>
2022-02-27 17:05:15 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
 import socket
 
+
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+print("press x to use default settings")
+print("enter Host: ")
+HOST = input("...")
+if PORT == "x" or HOST == "x":
+    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+    PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+print("enter port: ")
+PORT = input("...")
+if PORT == "x" or HOST == "x":
+    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+    PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
 
 #assumes running on localhost: i do not have two machines to test this on but in theory
 #if you want to run generally you just need to duplicate this and send between two servers too
